67b4c1e6ce46214029b1286fc334fdb45378e712
Igor0763 <Igor763763763@yandex.ru>
2023-11-07 17:44:47 +0400

    first commit

--- src/test/java/SoftAssertionsTest.java
+++ src/test/java/SoftAssertionsTest.java
@@ -29,11 +29,11 @@ public class SoftAssertionsTest {
 
 //     Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
-        $$("[href=\"/selenide/selenide/wiki/SoftAssertions\"]").first().shouldHave(text("SoftAssertion"));
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $(".gh-header-title").shouldHave(text("SoftAssertions"));
 
 
 //     Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5*/
-        $$("[href=\"/selenide/selenide/wiki/SoftAssertions\"]").first().click();
         $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
                 "class Tests {\n" +
                 "  @Test\n" +
